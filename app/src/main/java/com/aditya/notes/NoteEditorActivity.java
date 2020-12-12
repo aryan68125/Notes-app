97db3e007b782a41006ec31d46906d36b0d5e897
Aryan Kumar <aryan68125@gmail.com>
2020-12-12 21:44:22 +0530

    Version 4 (Text to speech engine added bug Fixes!!)

--- app/src/main/java/com/aditya/notes/NoteEditorActivity.java
+++ app/src/main/java/com/aditya/notes/NoteEditorActivity.java
@@ -23,7 +23,6 @@ public class NoteEditorActivity extends AppCompatActivity {
 
     //editText is responsible for editing notes
     EditText editText;
-    EditText editTextTitle;
 
     int noteId;
 
@@ -37,7 +36,6 @@ public class NoteEditorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_note_editor);
 
         editText = findViewById(R.id.editText);
-        editTextTitle = findViewById(R.id.editTextTitle);
 
         //getting the note id from the listView in Main Activity
         Intent intent = getIntent();
@@ -46,12 +44,10 @@ public class NoteEditorActivity extends AppCompatActivity {
         if(noteId!=-1){
             //this will set the text in the editText in noteEditorActivity from the listView in the Main activity
             editText.setText(MainActivity.notes.get(noteId));
-            editTextTitle.setText(MainActivity.title.get(noteId));
         }
         else{
             //if the noteId == -1 then add a new note in the list and that will be an empty string
             MainActivity.notes.add("");
-            MainActivity.title.add("");
             //picking a note Id since we added a new item onto there so it should be whatever the last Item is
             //size of an array is size -1
             noteId = MainActivity.notes.size() - 1;
@@ -78,6 +74,7 @@ public class NoteEditorActivity extends AppCompatActivity {
                 HashSet<String> hashSet = new HashSet<>(MainActivity.notes);
                 //hashset is something that can be stored in the SharedPreferences
                 sharedPreferences.edit().putStringSet("notes",hashSet).apply();
+                MainActivity.arrayAdapter.notifyDataSetChanged();
             }
 
             @Override
@@ -108,37 +105,6 @@ public class NoteEditorActivity extends AppCompatActivity {
             }
         });
 
-        //setting up the title a editText
-        editTextTitle.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                //this will handle the changes in the text of the EditText
-                //updating the Notes ArrayList from the Main Activity
-                //String.valueOf(s) converting CharSequence s into the string
-                MainActivity.title.set(noteId,String.valueOf(s));
-                //updating our arrayAdapter
-                MainActivity.arrayAdapter.notifyDataSetChanged();
-
-                //updating the notes shared preferences when we change anything in the notes Array
-                SharedPreferences titleSharedPreferences = getApplicationContext().getSharedPreferences("com.aditya.notes", Context.MODE_PRIVATE);
-                //here we will use sets and not serializer because we don't want the notes to be in particular order because there is no way to change the order of things
-                //converting this Arraylist notes into a hashset
-                HashSet<String> hashSet = new HashSet<>(MainActivity.title);
-                //hashset is something that can be stored in the SharedPreferences
-                titleSharedPreferences.edit().putStringSet("title",hashSet).apply();
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
     }
 
     //the code below will handle our menue in the title bar
@@ -158,7 +124,7 @@ public class NoteEditorActivity extends AppCompatActivity {
            if(everythingIsOKmttsIsGoodToGo==1){
                String text = editText.getText().toString();
                mtts.setPitch(1.1f); //setting up the pitch and speed of the speech in text to speech engine
-               mtts.setSpeechRate(0.85f);
+               mtts.setSpeechRate(1.1f);
                //making text to speech engine to speek our entered text
                //TextToSpeech.QUEUE_FLUSH = current txt is cancled to speak a new one
                //TextToSpeech.QUEUE_ADD the next text is spoken after the previous text is finished
